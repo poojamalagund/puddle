2e6e651776e667da8a9437e24de4f77ba5ec9ab1
Pooja Malagund <47099519+poojamalagund@users.noreply.github.com>
2024-10-15 20:50:45 +0530

    Add files via upload

---
+++ dashboard/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from item.models import Item,Category
+
+# Create your views here.
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+    categories = Category.objects.all()
+    return render(request, 'dashboard/index.html', {
+        'items': items,
+        'categories': categories,
+    }) 
+
